0690294677ddff8de127a874486b157939c57bb7
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-10-01 17:56:33 +0200

    constants

---
+++ assets/constants.py
@@ -0,0 +1,5 @@
+# assets/constants.py
+VALID_STREAM_TYPES = [
+        "time", "distance", "latlng", "altitude", "velocity_smooth", "heartrate", 
+        "cadence", "watts", "temp", "moving", "grade_smooth"
+    ]
