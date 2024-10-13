c9f72b92743c7c7408c5bf4dcdf58887fd82f11c
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-10-13 19:02:28 +0200

    added zombie note to self...

--- src/database/queries.py
+++ src/database/queries.py
@@ -1,18 +1,7 @@
+# src/database/queries.py
 import sqlite3
 import pandas as pd
 
-
-# def get_table_data(table_name: str, db_name: str):
-#     """
-#     Retrieves all the data from a given table and prints it.
-#     """
-
-#     conn = sqlite3.connect(f"data/{db_name}")
-#     query = f"SELECT * FROM {table_name}"
-#     df = pd.read_sql_query(query, conn)
-#     conn.close()
-
-#     return df
 
 def get_table_data(table_name: str, db_name: str):
     """
@@ -25,13 +14,13 @@ def get_table_data(table_name: str, db_name: str):
     try:
         # Connect to the SQLite database
         conn = sqlite3.connect(f"data/db_name")
-        
+
         # Query to get all data from the specified table
         query = f"SELECT * FROM {table_name}"
-        
+
         # Load the data into a DataFrame
         df = pd.read_sql_query(query, conn)
-        
+
         return df
     except sqlite3.Error as e:
         print(f"Error retrieving table data: {e}")
@@ -39,3 +28,11 @@ def get_table_data(table_name: str, db_name: str):
     finally:
         if conn:
             conn.close()
+
+
+# Query to join the databses for later:
+
+# SELECT a.*, w.temperature, w.precipitation
+
+# FROM activities a
+# JOIN weather w ON a.activity_id = w.activity_id;
