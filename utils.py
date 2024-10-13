847436a695104c17ff63281ad7c599d4a208eeae
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-10-13 16:30:03 +0200

    added timer function

--- utils.py
+++ utils.py
@@ -1,9 +1,24 @@
 # utils.py
+import time
 import pandas as pd
+from loguru import logger
 
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        logger.info(f"{func.__name__} executed in {execution_time:.1f} seconds")
+        return result
+    return wrapper
 
 def map_months(df: pd.DataFrame):
     df["month"] = df["month"].map(MONTH_MAPPING)
+    return df
+
+def map_weather_codes(df: pd.DataFrame):
+    df["weather_description"] = df["weather_code"].map(WEATHER_CODE_MAPPING)
     return df
 
 
@@ -45,14 +60,29 @@ WEATHER_CODE_MAPPING = {
     0: "Clear sky",
     1: "Mainly clear",
     2: "Partly cloudy",
-    3: "Cloudy",
-    4: "Overcast",
-    45: "Fog at a distance",
-    5: "Fog",
-    6: "Drizzle",
-    7: "Rain",
-    8: "Showers",
-    9: "Thunderstorms",
-    10: "Snow",
-    11: "Snow showers",
+    3: "Overcast",
+    45: "Fog",
+    48: "Depositing rime fog",
+    51: "Light drizzle",
+    53: "Moderate drizzle",
+    55: "Dense drizzle",
+    56: "Light freezing drizzle",
+    57: "Dense freezing drizzle",
+    61: "Slight rain",
+    63: "Moderate rain",
+    65: "Heavy rain",
+    66: "Light freezing rain",
+    67: "Heavy freezing rain",
+    71: "Slight snow fall",
+    73: "Moderate snow fall",
+    75: "Heavy snow fall",
+    77: "Snow grains",
+    80: "Slight rain showers",
+    81: "Moderate rain showers",
+    82: "Violent rain showers",
+    85: "Slight snow showers",
+    86: "Heavy snow showers",
+    95: "Thunderstorm",
+    96: "Thunderstorm with slight hail",
+    99: "Thunderstorm with heavy hail",
 }
